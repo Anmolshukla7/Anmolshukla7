32f1b90de2014f4fd01362ce7c1e9e775e1ae73c
ANMOL KUMAR <23it050@jssaten.ac.in>
2024-06-21 04:25:43 +0530

    Add files via upload

---
+++ algebra.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int 
+x=3;
+int y=4;
+printf("solve the equation %d\n",3*x/3-2*y);
+//left to right rule applied here
+//first 3 into x happens 
+//then divided by 3 happens
+//and 2 into y happens
+//at last substracted by 3*x/3-2*y
+return 0;
+}
